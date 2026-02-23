a46e28ff0fc7cc50d758a74ead71f61be5e91e9c
JP McNerney <jpmcnerney1@gmail.com>
2026-02-22 20:16:26 -0500

    hello world

---
+++ UKYCampusTour/UKYCampusTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  UKYCampusTourApp.swift
+//  UKYCampusTour
+//
+//  Created by JP McNerney on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct UKYCampusTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
